214e97d580de386f6c3670880346f65b654ca1d3
Usama Ali <imusamaali7@gmail.com>
2024-07-12 17:41:07 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Home from "./pages/Home";
+import Navebar from "./pages/Navebar";
+
+function App() {
+  return (
+    <>
+      <Navebar />
+      <Home />
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Home() {
+  return (
+    <>
+      <div className="flex  h-[100dvh]">
+        <div className="grow bg-blue-500 flex justify-center items-center">
+          1
+        </div>
+        <div className="grow bg-slate-600 flex justify-center items-center">
+          2
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Home;
