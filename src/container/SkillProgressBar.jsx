0ce399bdff57b89b7dc083b682168bf89aa1ee31
Usama Ali <imusamaali7@gmail.com>
2024-08-12 17:16:32 +0500

    Add CV button

--- src/container/SkillProgressBar.jsx
+++ src/container/SkillProgressBar.jsx
@@ -37,7 +37,7 @@ function SkillProgressBar() {
             viewport={{ once: true }}
           >
             <div className="flex justify-between my-2">
-              <p>JAVASCRIPT</p>
+              <p>Javascript</p>
               <p>80%</p>
             </div>
             <div className="bg-[#E9ECEF] w-full h-2 flex rounded-full  items-center">
@@ -51,8 +51,8 @@ function SkillProgressBar() {
             viewport={{ once: true }}
           >
             <div className="flex justify-between my-2">
-              <p>Reactjs</p>
-              <p>80%</p>
+              <p>React js</p>
+              <p>82%</p>
             </div>
             <div className="bg-[#E9ECEF] w-full h-2 flex rounded-full  items-center">
               <div className="bg-skin-color w-[80%] h-2 rounded-full"></div>
@@ -80,7 +80,7 @@ function SkillProgressBar() {
           >
             <div className="flex justify-between my-2 font-">
               <p>Node js/Express js</p>
-              <p>50%</p>
+              <p>70%</p>
             </div>
             <div className="bg-[#E9ECEF] w-full h-2 flex rounded-full  items-center">
               <div className="bg-skin-color w-[50%] h-2 rounded-full"></div>
@@ -108,16 +108,18 @@ function SkillProgressBar() {
         target="_blank"
         rel="noopener noreferrer"
       ></a>
-      <motion.button
-        className=" shadow-2xl w-44  md:mr-8 bg-transparent border-2 border-skin-color  text-textColor-color hover:bg-hover-color hover:text-white my-16 p-2 hover:ease"
-        initial={{ y: 50, opacity: 0 }}
-        whileInView={{ y: 0, opacity: 1 }}
-        transition={{ delay: 0.5, duration: 0.5 }}
-        viewport={{ once: true }}
-      >
-        {" "}
-        Download CV
-      </motion.button>
+      <a href="/resume.pdf" download="My_CV.pdf">
+        <motion.button
+          className=" shadow-2xl w-44  md:mr-8 bg-transparent border-2 border-skin-color  text-textColor-color hover:bg-hover-color hover:text-white my-16 p-2 hover:ease"
+          initial={{ y: 50, opacity: 0 }}
+          whileInView={{ y: 0, opacity: 1 }}
+          transition={{ delay: 0.5, duration: 0.5 }}
+          viewport={{ once: true }}
+        >
+          {" "}
+          Download CV
+        </motion.button>
+      </a>
     </>
   );
 }
